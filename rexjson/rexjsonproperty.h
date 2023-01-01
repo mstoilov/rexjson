f452e3ae83f1a3ff68a0f4ded4725ddb9ec3f5eb
Martin Stoilov <martin@sigmadrone.org>
2023-01-01 22:35:09 +0100

    New rexjson::property

--- rexjson/rexjsonproperty.h
+++ rexjson/rexjsonproperty.h
@@ -18,373 +18,227 @@
  *  Martin Stoilov <martin@sigmadrone.org>
  */
 
-#ifndef REXJSONPROPERTY_H_
-#define REXJSONPROPERTY_H_
+#ifndef REXJSONPROPERTYEX_H_
+#define REXJSONPROPERTYEX_H_
 
 #include <assert.h>
 #include <functional>
+#include <memory>
 #include "rexjson/rexjson++.h"
 
 namespace rexjson {
 
+/*
+* Convert void* to
+* rexjson::value 
+*/
+template<typename T>
+rexjson::value property_get(void* ctx)
+{
+	return *static_cast<T*>(ctx);
+}
+
+/*
+* Convert rexjson::value to plain type
+*/
+template<typename T>
+void property_set(const rexjson::value& val, void* ctx)
+{
+	val.get(*static_cast<T*>(ctx));
+}
+
 class property;
-using enumarate_children_callback = std::function<void(const std::string&, property&)>;
+using enumerate_children_callback = std::function<void(const std::string&, property&)>;
 
-
-typedef enum {readonly = 1, writeonly = 2, readwrite = 3} property_access;
-
-class iproperty_object {
+class iproperty {
 public:
-	virtual ~iproperty_object() { }
-	virtual iproperty_object* duplicate() const = 0;
-	virtual void set_prop(const rexjson::value& val) = 0;
-	virtual rexjson::value get_prop() = 0;
-	virtual void enumarate_children(property &parent, const std::string& path, const enumarate_children_callback& callback) = 0;
-	virtual property& navigate(property &parent, const std::string& path) = 0;
+	virtual ~iproperty() { }
+	virtual rexjson::value get() const = 0;
+	virtual void set(const rexjson::value& val) = 0;
+	virtual iproperty* clone() = 0;
+	virtual void insert(const std::string& name, const property& v) = 0;
+	virtual void enumerate(property& thisprop, const std::string& path, const enumerate_children_callback& callback) = 0;
+	virtual property& navigate(const std::string& path) = 0;
 	virtual property& operator[](const std::string& name) = 0;
-	virtual property& push_back(const property& v) = 0;
-	virtual property& insert(const std::string& name, const property& v) = 0;
-	virtual property_access access() const = 0;
 };
 
-
-template<typename T>
-class property_object : public iproperty_object {
+class property_leaf : public iproperty {
 public:
-	property_object() = delete;
-	property_object(
-			T* propaddr,
-			property_access access,
-			const std::function<void(const rexjson::value&)>& check_hook,
-			const std::function<void(void *ctx)>& modified_hook,
-			void* ctx)
-	: propaddr_(propaddr)
-	, access_(access)
-	, check_hook_(check_hook)
-	, modified_hook_(modified_hook)
-	, ctx_(ctx)
-	{
-		assert(propaddr_);
-	}
-
-	T* propaddr_;
-	property_access access_ = readwrite;
-	std::function<void(const rexjson::value&)> check_hook_ = {};
-	std::function<void(void *ctx)> modified_hook_ = {};
-	void* ctx_ = nullptr;
-
-	virtual iproperty_object* duplicate() const override
-	{
-		return new property_object(propaddr_, access_, check_hook_, modified_hook_, ctx_);
-	}
-
-	virtual void set_prop(const rexjson::value& val) override
-	{
-		if ((access_ & property_access::writeonly) == 0)
-			throw std::runtime_error("No write access");
-		check_hook_(val);
-		set_prop_impl<T>(propaddr_, val);
-		modified_hook_(ctx_);
-	}
-
-	virtual rexjson::value get_prop() override
-	{
-		if ((access_ & property_access::readonly) == 0)
-			throw std::runtime_error("No read access");
-		rexjson::value val = get_prop_impl(propaddr_);
-		return val;
-	}
-
-	virtual void enumarate_children(property &parent, const std::string& path, const enumarate_children_callback& callback) override
-	{
-		callback(path, parent);
-	}
-
-	virtual property& navigate(property &parent, const std::string& path) override
-	{
-		if (!path.empty())
-			throw (std::runtime_error("Invalid path"));
-		return parent;
-	}
-
-	virtual property& operator[](const std::string& name) override
-	{
-		throw std::runtime_error("property_object: Invalid call to operator[]");
-	}
-
-	virtual property& push_back(const property& v) override
-	{
-		throw std::runtime_error("property_object: Invalid call to push_back()");
-	}
-
-	virtual property& insert(const std::string& name, const property& v) override
-	{
-		throw std::runtime_error("property_object: Invalid call to insert()");
-	}
-
-	virtual property_access access() const override
-	{
-		return access_;
-	}
-
-	template <typename U>
-	typename std::enable_if<std::is_integral<U>::value || std::is_enum<U>::value, int64_t>::type
-	get_prop_impl(U* prop)
-	{
-		return *prop;
-	}
-
-	template <typename U>
-	typename std::enable_if<std::is_floating_point<U>::value, double>::type
-	get_prop_impl(U* prop)
-	{
-		return *prop;
-	}
-
-
-	template <typename U>
-	typename std::enable_if<std::is_base_of<std::string, U>::value, std::string>::type
-	get_prop_impl(U* prop)
-	{
-		return *prop;
-	}
-
-	template <typename U>
-	typename std::enable_if<std::is_integral<U>::value, void>::type
-	set_prop_impl(U* prop, const rexjson::value& val)
-	{
-		if (val.get_type() == rexjson::int_type) {
-			*prop = val.get_int64();
-		} else if (val.get_type() == rexjson::real_type) {
-			*prop = (U)val.get_real();
-		} else if (val.get_type() == rexjson::bool_type) {
-			*prop = val.get_bool() ? 1 : 0;
-		} else {
-			throw std::runtime_error("Invalid property type.");
-		}
-	}
-
-	template <typename U>
-	typename std::enable_if<std::is_floating_point<U>::value, void>::type
-	set_prop_impl(U* prop, const rexjson::value& val)
-	{
-		*prop = val.get_real();
-	}
-
-	template <typename U>
-	typename std::enable_if<std::is_base_of<std::string, U>::value, void>::type
-	set_prop_impl(U* prop, const rexjson::value& val)
-	{
-		*prop = val.get_str();
-	}
-
-	template <typename U>
-	typename std::enable_if<std::is_enum<U>::value, void>::type
-	set_prop_impl(U* prop, const rexjson::value& val)
-	{
-		*reinterpret_cast<typename std::underlying_type<U>::type*>(prop) = val.get_int();
-	}
-};
-
-template<>
-class property_object<bool> : public iproperty_object {
-public:
-	property_object(
-			bool* propaddr,
-			property_access access = property_access::readwrite,
-			const std::function<void(const rexjson::value&)>& check_hook = {},
-			const std::function<void(void *ctx)>& modified_hook = {},
-			void *ctx = nullptr)
-	: propaddr_(propaddr)
-	, access_(access)
-	, check_hook_(check_hook)
-	, modified_hook_(modified_hook)
-	, ctx_(ctx)
-	{}
-
-	bool* propaddr_;
-	property_access access_ = readwrite;
-	std::function<void(const rexjson::value&)> check_hook_ = {};
-	std::function<void(void *ctx)> modified_hook_ = {};
-	void *ctx_ = nullptr;
-
-
-	virtual iproperty_object* duplicate() const override
-	{
-		return new property_object(propaddr_, access_, check_hook_, modified_hook_, ctx_);
-	}
-
-	virtual void set_prop(const rexjson::value& val) override
-	{
-		if ((access_ & property_access::writeonly) == 0)
-			throw std::runtime_error("No write access");
-		check_hook_(val);
-		*propaddr_ = val.get_bool();
-		modified_hook_(ctx_);
-	}
-
-	virtual rexjson::value get_prop() override
-	{
-		if ((access_ & property_access::readonly) == 0)
-			throw std::runtime_error("No read access");
-		return *propaddr_;
-	}
-
-	virtual void enumarate_children(property &parent, const std::string& path, const enumarate_children_callback& callback) override
-	{
-		callback(path, parent);
-	}
-
-	virtual property& navigate(property &parent, const std::string& path) override
-	{
-		if (!path.empty())
-			throw (std::runtime_error("Invalid path"));
-		return parent;
-	}
-
-	virtual property& operator[](const std::string& name) override
-	{
-		throw std::runtime_error("property_object: Invalid call to operator[]");
-	}
-
-	virtual property& push_back(const property& v) override
-	{
-		throw std::runtime_error("property_object: Invalid call to push_back()");
-	}
-
-	virtual property& insert(const std::string& name, const property& v) override
-	{
-		throw std::runtime_error("property_object: Invalid call to method insert()");
-	}
-
-	virtual property_access access() const override
-	{
-		return access_;
-	}
-
-};
-
-template<typename T>
-class property_range : public property_object<T> {
-public:
-	using base = property_object<T>;
-
-	property_range() = delete;
-	property_range(
-			T* propaddr,
-			size_t size,
-			property_access access,
-			const std::function<void(const rexjson::value&)>& check_hook,
-			const std::function<void(void *ctx)>& modified_hook,
-			void* ctx)
-	: property_object<T>(propaddr, access, check_hook, modified_hook, ctx)
-	, size_(size)
+	property_leaf(void* ctx, const std::function<rexjson::value (void* ctx)>& getter, const std::function<void(const rexjson::value& value, void* ctx)>& setter)
+		: ctx_(ctx), getter_(getter), setter_(setter)
 	{
 	}
 
-	virtual iproperty_object* duplicate() const
+	property_leaf(void* ctx, const std::function<const rexjson::value& (void* ctx)>& getter)
+		: ctx_(ctx), getter_(getter)
 	{
-		return new property_range(base::propaddr_, size_, base::access_, base::check_hook_, base::modified_hook_, base::ctx_);
 	}
 
-	virtual void set_prop(const rexjson::value& val) override
+	property_leaf(void* ctx, const std::function<void(const rexjson::value& value, void* ctx)>& setter)
+		: ctx_(ctx), setter_(setter)
 	{
-		if ((base::access_ & property_access::writeonly) == 0)
-			throw std::runtime_error("No write access");
-		base::check_hook_(val);
-		size_t i = 0;
-		const rexjson::array& a = val.get_array();
-		for (auto v : a) {
-			if (i >= size_)
-				throw std::range_error("The array of values passed is of bigger size than the property range.");
-			base::set_prop_impl(base::propaddr_ + i, v);
-			++i;
-		}
-		base::modified_hook_(base::ctx_);
 	}
 
-	virtual rexjson::value get_prop() override
+	virtual rexjson::value get() const override
 	{
-		rexjson::array ret;
-		for (size_t i = 0; i < size_; i++) {
-			ret.push_back(base::get_prop_impl(base::propaddr_ + i));
-		}
+		if (!getter_) 
+			throw std::runtime_error("No read operation defined for the property.");
+		rexjson::value ret = getter_(ctx_);
+		std::cout << "get(): " << ret.to_string() << std::endl;
 		return ret;
+
+	}
+
+	virtual void set(const rexjson::value& value) override
+	{
+		if (!setter_) 
+			throw std::runtime_error("No write operation defined for the property.");
+		setter_(value, ctx_);
+	}
+
+	virtual iproperty* clone() override
+	{
+		return new property_leaf(ctx_, getter_, setter_);
+	}
+
+	virtual void insert(const std::string& name, const property& v) override
+	{
+		throw std::runtime_error("insert not implemented.");
+	}
+
+	virtual void enumerate(property& thisprop, const std::string& path, const enumerate_children_callback& callback) override
+	{
+		callback(path, thisprop);
+	}
+
+	virtual property& navigate(const std::string& path) override
+	{
+		throw std::runtime_error("navigate not implemented.");
+	}
+
+	virtual property& operator[](const std::string& name) override
+	{
+		throw std::runtime_error("operator[] not implemented.");
 	}
 
 
-protected:
-	size_t size_ = 0;
+	void* ctx_;
+	std::function<rexjson::value (void* ctx)> getter_;
+	std::function<void(const rexjson::value& value, void* ctx)> setter_;
 };
+
 class property_map;
-class property_array;
 
 class property {
 public:
-	virtual ~property();
-	template<typename T>
-	property(
-			T* propaddr,
-			property_access access = property_access::readwrite,
-			const std::function<void(const rexjson::value&)>& check_hook = [](const rexjson::value& v)->void{},
-			const std::function<void(void *ctx)>& modified_hook = [](void *ctx)->void{},
-			void *ctx = nullptr)
-	{
-		object_ = static_cast<iproperty_object*>(new property_object<T>(propaddr, access, check_hook, modified_hook, ctx));
-	}
-
-	template<typename T>
-	property(
-			T* propaddr,
-			size_t size,
-			property_access access = property_access::readwrite,
-			const std::function<void(const rexjson::value&)>& check_hook = [](const rexjson::value& v)->void{},
-			const std::function<void(void *ctx)>& modified_hook = [](void *ctx)->void{},
-			void *ctx = nullptr)
-	{
-		object_ = static_cast<iproperty_object*>(new property_range<T>(propaddr, size, access, check_hook, modified_hook, ctx));
-	}
-
-	property();
-	property(const property& v);
-	property(property&& v);
+	property() = default;
 	property(const property_map& map);
-	property(const property_array& array);
-	property& push_back(const property& v);
-	property& insert(const std::string& name, const property& v);
-	property& operator[](size_t i);
-	property& operator[](const std::string& name);
-	property& operator=(const property& v);
-	property& navigate(const std::string& path);
-	void set_prop(const rexjson::value& val);
-	rexjson::value get_prop();
-	property_access access() const;
-	void enumerate_children(const std::string& path, const enumarate_children_callback& callback);
-	void check_object();
-	rexjson::value to_json();
+	property(void* ctx, const std::function<rexjson::value (void* ctx)>& getter, const std::function<void(const rexjson::value& value, void* ctx)>& setter)
+		: property_object_(new property_leaf(ctx, getter, setter))
+	{
+	}
 
+	property(void* ctx, const std::function<const rexjson::value& (void* ctx)>& getter)
+		: property_object_(new property_leaf(ctx, getter))
+	{
+	}
+
+	property(void* ctx, const std::function<void(const rexjson::value& value, void* ctx)>& setter)
+		: property_object_(new property_leaf(ctx, setter))
+	{
+	}
+
+	property(const property& prop) 
+	{
+		property_object_.reset(prop.property_object_->clone());
+	}
+
+	property& operator=(const property& prop)
+	{
+		property_object_.reset(prop.property_object_->clone());
+		return *this;
+	}
+
+	rexjson::value get() const
+	{
+		return property_object_->get();
+	}
+
+	void set(const rexjson::value& value) 
+	{
+		property_object_->set(value);
+	}
+
+
+	void insert(const std::string& name, const property& v)
+	{
+		property_object_->insert(name, v);
+	}
+
+	void enumerate(property& thisprop, const std::string& path, const enumerate_children_callback& callback)
+	{
+		property_object_->enumerate(thisprop, path, callback);
+	}
+
+	property& navigate(const std::string& path)
+	{
+		return property_object_->navigate(path);
+	}
+
+	property& operator[](const std::string& name)
+	{
+		return property_object_->operator[](name);
+	}
+
+	value to_json()
+	{
+		rexjson::object ret;
+		enumerate(*this, "", [&](const std::string& path, rexjson::property& prop)->void {
+			ret[path] = prop.get();
+		});
+		return ret;
+	}
 
 protected:
-	iproperty_object* object_;
+	std::unique_ptr<iproperty> property_object_;
 };
 
-class property_map : public iproperty_object {
+class property_map : public iproperty {
+protected:
+	std::map<std::string, property> map_;
+
 public:
 	property_map() = default;
 	property_map(std::initializer_list<typename std::map<std::string, property>::value_type>lst) : map_(lst) {}
 	property_map(const property_map& map) : map_(map.map_) {}
-	virtual ~property_map() { }
-	virtual iproperty_object* duplicate() const override { return new property_map(*this); }
-	virtual void set_prop(const rexjson::value& val) override { throw std::runtime_error("property_map: Illegal SetProp() call."); }
-	virtual rexjson::value get_prop() override { throw std::runtime_error("property_map: Illegal GetProp() call."); }
-	virtual void enumarate_children(property &parent, const std::string& path, const enumarate_children_callback& callback) override
+	virtual rexjson::value get() const override
+	{
+		throw std::runtime_error("get not implemented.");
+	}
+
+	virtual void set(const rexjson::value& value) override
+	{
+		throw std::runtime_error("set not implemented.");
+	}
+
+	virtual iproperty* clone() override
+	{
+		return new property_map(*this);
+	}
+
+	virtual void insert(const std::string& name, const property& v) override
+	{
+		map_[name] = property(v);
+	}
+
+	virtual void enumerate(property& thisprop, const std::string& path, const enumerate_children_callback& callback) override
 	{
 		for (auto &o : map_) {
-			o.second.enumerate_children(path + "." + o.first, callback);
+			o.second.enumerate(o.second, path + "." + o.first, callback);
 		}
 	}
 
-	virtual property& navigate(property &parent, const std::string& path) override
+	virtual property& navigate(const std::string& path) override
 	{
 		std::string toc = path;
 		std::size_t toc_pos = toc.find_first_of(".[", 1);
@@ -396,7 +250,10 @@ public:
 			throw (std::runtime_error("Invalid path"));
 		std::string restpath = path.substr(toc.size());
 		std::string trimmed = (toc.size() && toc.at(0) == '.') ? toc.substr(1) : toc;
-		return (operator [](trimmed)).navigate(restpath);
+		if (restpath.empty())
+			return operator [](trimmed);
+		else
+			return (operator [](trimmed)).navigate(restpath);
 	}
 
 	virtual property& operator[](const std::string& name) override
@@ -405,89 +262,15 @@ public:
 		if (it == map_.end())
 			throw std::runtime_error("Invalid property: " + name);
 		return it->second;
-	}
-
-	virtual property& push_back(const property& v) override
-	{
-		throw std::runtime_error("property_map: Invalid call to push_back()");
-	}
-
-	virtual property& insert(const std::string& name, const property& v) override
-	{
-		auto ret = map_.insert(std::pair<std::string, property>(name, v));
-		return ret.first->second;
-	}
-
-	virtual property_access access() const override
-	{
-		throw std::runtime_error("Invalid call to method access()");
-	}
-
-
-protected:
-	std::map<std::string, property> map_;
+	}	
 };
 
-class property_array : public iproperty_object {
-public:
-	property_array() = default;
-	property_array(std::initializer_list<property>lst) : array_(lst) {}
-	property_array(const property_array& array) : array_(array.array_) {}
-	virtual ~property_array() { }
-	virtual iproperty_object* duplicate() const override { return new property_array(*this); }
-	virtual void set_prop(const rexjson::value& val) override { throw std::runtime_error("property_array: Illegal SetProp() call."); }
-	virtual rexjson::value get_prop() override { throw std::runtime_error("property_array: Illegal GetProp() call."); }
-	virtual void enumarate_children(property &parent, const std::string& path, const enumarate_children_callback& callback) override
-	{
-		size_t i = 0;
-		for (auto &o : array_) {
-			o.enumerate_children(path + "[" + std::to_string(i++) + "]", callback);
-		}
-	}
+inline property::property(const property_map& map)
+{
+	property_object_.reset(new property_map(map));
+}	
 
-	virtual property& navigate(property &parent, const std::string& path) override
-	{
-		std::string toc = path;
-		if (!toc.size() || toc.at(0) != '[')
-			throw (std::runtime_error("Invalid path"));
-		std::size_t toc_pos = toc.find_first_of("]", 1);
-		if (toc_pos == std::string::npos)
-			throw (std::runtime_error("Invalid path"));
-		toc = toc.substr(1, toc_pos - 1);
-		if (toc.empty())
-			throw (std::runtime_error("Invalid path"));
-		std::string restpath = path.substr(toc.size() + 2);
-		return (operator [](toc)).navigate(restpath);
-	}
 
-	virtual property& operator[](const std::string& name) override
-	{
-		size_t idx = atol(name.c_str());
-		if (idx >= array_.size())
-			throw std::range_error("Invalid index");
-		return array_.operator [](idx);
-	}
+}
 
-	virtual property& push_back(const property& v) override
-	{
-		array_.push_back(v);
-		return *array_.rbegin();
-	}
-
-	virtual property& insert(const std::string& name, const property& v) override
-	{
-		throw std::runtime_error("Invalid call to method insert()");
-	}
-
-	virtual property_access access() const override
-	{
-		throw std::runtime_error("Invalid call to method access()");
-	}
-
-protected:
-	std::vector<property> array_;
-};
-
-} // namespace rexjson
-
-#endif /* REXJSONPROPERTY_H_ */
+#endif
